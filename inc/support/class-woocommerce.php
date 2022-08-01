c890e1b91c0321373763c95af59a936766c94fa3
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2022-08-01 19:36:04 +0300

    ss

--- inc/support/class-woocommerce.php
+++ inc/support/class-woocommerce.php
@@ -52,8 +52,14 @@ class WooCommerce {
 		add_action( 'woocommerce_save_product_variation', 				[ $this, 'save_product_variation' 		], 10, 2 );
 		add_filter( 'woocommerce_available_variation', 					[ $this, 'available_variation' 			] );
 
+		add_filter( 'default_checkout_billing_state', 					[ $this, 'change_default_checkout_state' ] );
+		add_filter( 'default_checkout_shipping_state', 					[ $this, 'change_default_checkout_state' ] );
 		// add_filter( 'woocommerce_default_address_fields',				[ $this, 'default_address_fields' ], 	10, 1 );
 		add_filter( 'woocommerce_checkout_fields',						[ $this, 'custom_checkout_fields' ], 	10, 1 );
+	}
+
+	public function change_default_checkout_state() {
+		return 'GJ'; // state code
 	}
 		
 	public function default_address_fields( $states ) {
@@ -64,7 +70,7 @@ class WooCommerce {
 
 	public function custom_checkout_fields( $fields ) {
 		// unset( $fields['billing']['billing_country'] );
-		unset( $fields['billing']['billing_state'] );
+		// unset( $fields['billing']['billing_state'] );
 
 		$fields['billing']['billing_city']['custom_attributes']['readonly'] = 'true';
 		$fields['billing']['billing_city']['value'] = 'Targu Jiu';
