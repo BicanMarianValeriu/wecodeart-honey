8bd0ffe36c031e4189ce9af1c3d5c6870ff1e747
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2022-11-13 21:10:34 +0200

    WOO

--- inc/support/class-woocommerce.php
+++ inc/support/class-woocommerce.php
@@ -60,21 +60,25 @@ class WooCommerce {
 		add_filter( 'woocommerce_states', 								[ $this, 'custom_woocommerce_state' ], 	10, 1 );
 
 		// Fragment Cache
-		var_dump( apply_filters( 'litespeed_esi_status', false ) );
+		add_action( 'render_block', function( $content, $data ) {
+			if( get_prop( $data, [ 'blockName' ] ) === 'woocommerce/mini-cart' ) {
+				return apply_filters( 'litespeed_esi_url', 'woo-mini-cart', 'WECODEART_WOO_ESI_CART', [
+					'content' 	=> $content,
+					'data'		=> $data
+				] );
+			}
 
-		add_action( 'wp_footer', function() {
-			return apply_filters( 'litespeed_esi_url', 'woo-mini-cart', 'WECODEART_WOO_ESI_CART', [
-				'content' 	=> 'Test',
-				'data'		=> []
-			] );
+			return $content;
 		}, 10, 2 );
-
+		
 		add_action( 'litespeed_esi_load-woo-mini-cart', function( $params ) {
 			do_action( 'litespeed_control_set_private', 'cache cart' );
 			do_action( 'litespeed_vary_no' );
 			do_action( 'litespeed_tag_add_private_esi', 'woo-mini-cart' );
 
-			echo 'Hello world: ' . rand(1, 10);
+			var_dump( apply_filters( 'litespeed_esi_status', false ) );
+
+			echo $params['content'];
 		} );
 	}
 
