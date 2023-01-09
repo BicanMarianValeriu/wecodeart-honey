09e113453c3b987c1530315ebf0704651371ff53
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2023-01-09 21:24:18 +0200

    CSS

--- assets/minified/js/frontend.min.asset.php
+++ assets/minified/js/frontend.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '3ce3f7efc0ce8e19092f');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'eae1ea68183a7e861df6');
