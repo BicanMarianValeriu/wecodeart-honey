e84ba0d7cf4882490ad827405b313b8ba8f11fd6
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2023-06-20 00:18:30 +0300

    adjust patterns

--- patterns/latest-posts.php
+++ patterns/latest-posts.php
@@ -8,10 +8,10 @@
 <!-- wp:group {"className":"py-5","layout":{"inherit":true}} -->
 <div class="wp-block-group py-5">
     <!-- wp:heading {"textAlign":"center","level":4,"textColor":"primary","className":"mb-0"} -->
-    <h4 class="has-text-align-center mb-0 has-primary-color has-text-color" id="our-block">Blog-ul nostru</h4>
+    <h4 class="has-text-align-center mb-0 has-primary-color has-text-color" id="our-blog"><?php esc_html_e( 'Our Blog', 'wecodeart-honey' ); ?></h4>
     <!-- /wp:heading -->
     <!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"0"}}},"textColor":"dark","className":"h1"} -->
-    <h2 class="wp-block-heading has-text-align-center h1 has-dark-color has-text-color" id="latest-posts" style="margin-top:0">Ultimele postări</h2>
+    <h2 class="wp-block-heading has-text-align-center h1 has-dark-color has-text-color" id="latest-posts" style="margin-top:0"><?php esc_html_e( 'Latest Posts', 'wecodeart-honey' ); ?></h2>
     <!-- /wp:heading -->
     <!-- wp:separator {"color":"primary","className":"is-style-dots"} -->
     <hr class="wp-block-separator has-text-color has-background has-primary-background-color has-primary-color is-style-dots" />
@@ -22,13 +22,13 @@
         <!-- wp:group {"backgroundColor":"light","className":"p-3 p-sm-4 rounded-2 h-100"} -->
         <div class="wp-block-group p-3 p-sm-4 rounded-2 h-100 has-light-background-color has-background">
             <!-- wp:post-featured-image {"isLink":true,"className":"rounded-2"} /-->
-            <!-- wp:group {"className":"gap-1 mb-3","layout":{"type":"flex","justifyContent":"center"}} -->
-            <div class="wp-block-group gap-1 mb-3">
+            <!-- wp:group {"className":"g-1","layout":{"type":"flex","justifyContent":"center"}} -->
+            <div class="wp-block-group g-1">
                 <!-- wp:pattern {"slug":"wecodeart/el-entry-meta"} /-->
             </div>
             <!-- /wp:group -->
-            <!-- wp:post-title {"textAlign":"center","level":3,"isLink":true,"className":"fw-700 mb-1"} /-->
-            <!-- wp:post-excerpt {"textAlign":"center","moreText":"Vezi mai mult"} /-->
+            <!-- wp:post-title {"textAlign":"center","level":3,"isLink":true,"className":"fw-700"} /-->
+            <!-- wp:post-excerpt {"textAlign":"center","moreText":""} /-->
         </div>
         <!-- /wp:group -->
         <!-- /wp:post-template -->
--- patterns/latest-products.php
+++ patterns/latest-products.php
@@ -8,10 +8,10 @@
 <!-- wp:group {"className":"py-5","layout":{"inherit":true}} -->
 <div class="wp-block-group py-5">
     <!-- wp:heading {"textAlign":"center","level":4,"textColor":"primary","className":"mb-0"} -->
-    <h4 class="has-text-align-center mb-0 has-primary-color has-text-color" id="our-shop">Ultimele</h4>
+    <h4 class="has-text-align-center mb-0 has-primary-color has-text-color" id="our-shop"><?php esc_html_e( 'Latest', 'wecodeart-honey' ); ?></h4>
     <!-- /wp:heading -->
     <!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"0"}}},"textColor":"dark","className":"h1"} -->
-    <h2 class="wp-block-heading has-text-align-center h1 has-dark-color has-text-color" id="latest-products" style="margin-top:0">Bunătăți</h2>
+    <h2 class="wp-block-heading has-text-align-center h1 has-dark-color has-text-color" id="latest-products" style="margin-top:0"><?php esc_html_e( 'Sweets', 'wecodeart-honey' ); ?></h2>
     <!-- /wp:heading -->
     <!-- wp:separator {"backgroundColor":"primary","className":"is-style-dots"} -->
     <hr class="wp-block-separator has-text-color has-primary-color has-alpha-channel-opacity has-primary-background-color has-background is-style-dots" />
@@ -27,7 +27,7 @@
     <div class="wp-block-buttons">
         <!-- wp:button {"backgroundColor":"primary","className":"is-style-fill"} -->
         <div class="wp-block-button is-style-fill">
-            <a class="wp-block-button__link has-primary-background-color has-background" href="#">Vezi Mai Multe</a>
+            <a class="wp-block-button__link has-primary-background-color has-background" href="#"><?php esc_html_e( 'See More', 'wecodeart-honey' ); ?></a>
         </div>
         <!-- /wp:button -->
     </div>
